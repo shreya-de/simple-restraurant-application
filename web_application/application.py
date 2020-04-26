2da215615fe06bf5d04a847770e808e7ce6ccd12
Shreya De <shreyade833@gmail.com>
2020-04-26 14:05:32 +0530

    web_application/application.py

--- web_application/application.py
+++ web_application/application.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask("shreya")
-
-@app.route("/")
-def homepage():
-    return "Hello World"
+food_items=[["Chicken", 20], ["Pizza", 30], ["meat", 50]]
 
 @app.route("/a")
-def body():
-    return render_template("webpage.html") 
+def getFoodItems():
+    return render_template("webpage.html", food_items=food_items)
 
+@app.route("/c")
+def deleteItem():
+    item_index=int(request.args.get('item_index'))
+    del food_items[item_index-1]
+    return "Item deleted at index :%s"  % item_index
+    
 app.run()
